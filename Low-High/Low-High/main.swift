ed78f2075f7ae17d21b38ec18ff93cebcc1b74f2
ridgeracer <ridgeracer@nate.com>
2020-04-26 15:04:20 +0900

    xcode- setting done

---
+++ Low-High/Low-High/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Low-High
+//
+//  Created by ridgeracer on 26/04/2020.
+//  Copyright © 2020 ridgeracer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
