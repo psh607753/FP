0c3e0f6511cde76e73c9e190a55ddfa911e7b4e2
ridgeracer <ridgeracer@nate.com>
2020-04-26 16:10:49 +0900

    step 0 : basic

--- Low-High/Low-High/main.swift
+++ Low-High/Low-High/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-print("Hello, World!")
+let answer = Int(arc4random() % 100) + 1
+var count = 0
 
+while true {
+
+    let userInput = readLine()
+
+    guard let unwrappedInput = userInput, let inputNumber = Int(unwrappedInput) else {
+        print("Wrong")
+        continue
+    }
+
+    if inputNumber == answer {
+        print("Correct! : \(count)")
+        break
+    }
+
+    if inputNumber > answer {
+        print("High")
+    }
+
+    if inputNumber < answer {
+        print("Low")
+    }
+
+    count += 1
+}
